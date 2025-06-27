bf7881827b07ae7b7ebb513f893512f8f69a47bc
Khushi <skhushi1411@gmail.com>
2025-06-27 20:36:47 +0530

    resolving deployment error 2

--- actions/getActiveProductsWithPrices.ts
+++ actions/getActiveProductsWithPrices.ts
@@ -1,19 +1,41 @@
-import { ProductWithPrice } from '@/types';
+// import { ProductWithPrice } from '@/types';
+// import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+// import { cookies } from 'next/headers';
+
+// export const getActiveProductsWithPrices = async (): Promise<ProductWithPrice[]> => {
+//   const supabase = createServerComponentClient({
+//     cookies: cookies,
+//   });
+
+//   const { data, error } = await supabase
+//     .from('products')
+//     .select('*, prices(*)')
+//     .eq('active', true)
+//     .eq('prices.active', true)
+//     .order('metadata->index')
+//     .order('unit_amount', { foreignTable: 'prices' });
+
+//   if (error) {
+//     console.log(error);
+//   }
+
+//   return (data as any) || [];
+// };
+
+import { Product } from '@/types';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
-export const getActiveProductsWithPrices = async (): Promise<ProductWithPrice[]> => {
+export const getActiveProductsWithPrices = async (): Promise<Product[]> => {
   const supabase = createServerComponentClient({
     cookies: cookies,
   });
 
   const { data, error } = await supabase
     .from('products')
-    .select('*, prices(*)')
+    .select('*')
     .eq('active', true)
-    .eq('prices.active', true)
-    .order('metadata->index')
-    .order('unit_amount', { foreignTable: 'prices' });
+    .order('metadata->index');
 
   if (error) {
     console.log(error);
